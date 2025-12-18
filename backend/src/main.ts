d4874df125976871df0bc3203fae5e3256240714
issosnj <snj@issousa.org>
2025-12-18 11:19:50 -0500

    Remove unused cors import

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
