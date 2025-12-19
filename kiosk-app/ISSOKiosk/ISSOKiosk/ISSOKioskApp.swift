45702fabfbe7952e167aa90ba86b541890ff0616
issosnj <snj@issousa.org>
2025-12-19 12:09:37 -0500

    Temporarily comment Square SDK import until package is added
    - Commented out Square SDK import to fix build error
    - Added instructions for adding Square SDK package
    - Code will work with backend payment processing
    - Uncomment after adding package in Xcode

--- kiosk-app/ISSOKiosk/ISSOKiosk/ISSOKioskApp.swift
+++ kiosk-app/ISSOKiosk/ISSOKiosk/ISSOKioskApp.swift
@@ -1,5 +1,6 @@
 import SwiftUI
-import SquareInAppPaymentsSDK
+// Uncomment after adding Square SDK package in Xcode
+// import SquareInAppPaymentsSDK
 
 @main
 struct ISSOKioskApp: App {
@@ -7,13 +8,16 @@ struct ISSOKioskApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     init() {
-        // Initialize Square SDK
-        if let appId = Bundle.main.object(forInfoDictionaryKey: "SQUARE_APPLICATION_ID") as? String {
-            SQIPInAppPaymentsSDK.squareApplicationID = appId
-            print("[Square SDK] Initialized with Application ID: \(appId.prefix(8))...")
-        } else {
-            print("[Square SDK] Warning: SQUARE_APPLICATION_ID not found in Info.plist")
-        }
+        // Initialize Square SDK (uncomment after adding package)
+        // if let appId = Bundle.main.object(forInfoDictionaryKey: "SQUARE_APPLICATION_ID") as? String {
+        //     SQIPInAppPaymentsSDK.squareApplicationID = appId
+        //     print("[Square SDK] Initialized with Application ID: \(appId.prefix(8))...")
+        // } else {
+        //     print("[Square SDK] Warning: SQUARE_APPLICATION_ID not found in Info.plist")
+        // }
+        
+        // For now, payments are processed through backend
+        print("[Square SDK] Using backend payment processing")
     }
     
     var body: some Scene {
