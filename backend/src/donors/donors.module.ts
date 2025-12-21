f49bc5907c1e4cbfe56e81aa1b383fc66f92f0ed
issosnj <snj@issousa.org>
2025-12-21 12:56:49 -0500

    Fix DonorsModule dependency injection for DeviceAuthGuard

--- backend/src/donors/donors.module.ts
+++ backend/src/donors/donors.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 import { Donor } from './entities/donor.entity';
 import { DonorsService } from './donors.service';
 import { DonorsController } from './donors.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Donor])],
+  imports: [
+    TypeOrmModule.forFeature([Donor]),
+    JwtModule.register({}), // Required for DeviceAuthGuard
+  ],
   controllers: [DonorsController],
   providers: [DonorsService],
   exports: [DonorsService],
